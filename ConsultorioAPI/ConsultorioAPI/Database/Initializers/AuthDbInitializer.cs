7e443c7688d69d60cd48d4e5dba578143641a18e
serramatutu <lucasvvop@hotmail.com>
2017-10-24 10:58:04 -0200

    Criado projeto do visual

---
+++ ConsultorioAPI/ConsultorioAPI/Database/Initializers/AuthDbInitializer.cs
@@ -0,0 +1,20 @@
+﻿using ConsultorioAPI.Database.Contexts;
+using ConsultorioAPI.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ConsultorioAPI.Database.Initializers
+{
+    public class AuthDbInitializer : DropCreateDatabaseAlways<AuthContext>
+    {
+        protected override void Seed(AuthContext context)
+        {
+            var usermanager = new UserManager<UserModel>(new UserStore<UserModel>(context));
+        }
+    }
+}
