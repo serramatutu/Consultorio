503810e8d4a145488c56080796db299a26ec291c
DaviOliveiraSilva <davicampinas@hotmail.com>
2017-11-21 10:51:23 -0200

    Página de alteração de conta atualizada

--- Site/app/paciente/controllers/contaController.js
+++ Site/app/paciente/controllers/contaController.js
@@ -1,5 +1,98 @@
 ﻿'use strict';
-app.controller('contaController', ['$scope', function ($scope) {
+app.controller('contaController', ['$scope', '$uibModal', function ($scope, $modal) {
+
+    $scope.open = function (modal) {
+        switch (modal) {
+            case 'nome':
+                $scope.modalInstance = $modal.open({
+                    ariaLabelledBy: 'modal-title',
+                    ariaDescribedBy: 'modal-body',
+                    templateUrl: 'Nome.html',
+                    controller: 'contaController'
+                });
+                break;
+            case 'email':
+                $scope.modalInstance = $modal.open({
+                    ariaLabelledBy: 'modal-title',
+                    ariaDescribedBy: 'modal-body',
+                    templateUrl: 'Email.html',
+                    controller: 'contaController'
+                });
+                break;
+            case 'telefone':
+                $scope.modalInstance = $modal.open({
+                    ariaLabelledBy: 'modal-title',
+                    ariaDescribedBy: 'modal-body',
+                    templateUrl: 'Telefone.html',
+                    controller: 'contaController'
+                });
+                break;
+            case 'celular':
+                $scope.modalInstance = $modal.open({
+                    ariaLabelledBy: 'modal-title',
+                    ariaDescribedBy: 'modal-body',
+                    templateUrl: 'Celular.html',
+                    controller: 'contaController'
+                });
+                break;
+            case 'endereco':
+                $scope.modalInstance = $modal.open({
+                    ariaLabelledBy: 'modal-title',
+                    ariaDescribedBy: 'modal-body',
+                    templateUrl: 'Endereco.html',
+                    controller: 'contaController'
+                });
+                break;
+            case 'senha':
+                $scope.modalInstance = $modal.open({
+                    ariaLabelledBy: 'modal-title',
+                    ariaDescribedBy: 'modal-body',
+                    templateUrl: 'Senha.html',
+                    controller: 'contaController'
+                });
+                break;
+            case 'username':
+                $scope.modalInstance = $modal.open({
+                    ariaLabelledBy: 'modal-title',
+                    ariaDescribedBy: 'modal-body',
+                    templateUrl: 'UserName.html',
+                    controller: 'contaController'
+                });
+                break;
+        }
+    }
+
+    $scope.cancel = function(){
+        $scope.$close();
+    }
+
+    $scope.ok = function(campo){
+        switch (campo) {
+            case 'nome':
+                console.log(document.getElementsByName('value')[0].value);
+                break;
+            case 'email':
+                console.log(document.getElementsByName('value')[0].value);
+                break;
+            case 'telefone':
+                console.log(document.getElementsByName('value')[0].value);
+                break;
+            case 'celular':
+                console.log(document.getElementsByName('value')[0].value);
+                break;
+            case 'endereco':
+                console.log(document.getElementsByName('value')[0].value);
+                break;
+            case 'senha':
+                console.log(document.getElementsByName('value')[0].value);
+                console.log(document.getElementsByName('confValue')[0].value);
+                break;
+            case 'username':
+                console.log(document.getElementsByName('value')[0].value);
+        }
+
+    }
+
     $scope.savedSuccessfully = false;
     $scope.message = "";
 
